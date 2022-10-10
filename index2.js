62cd54e904fd1ddb24def43a83bd90c0ec89178b
Adarsh singh <adarshsingh345125@gmail.com>
2022-10-10 14:50:54 +0530

    ytdownloadFrontend

---
+++ index2.js
@@ -0,0 +1,14 @@
+console.log("in index2.js");
+var button =document.querySelector('.button')
+var text =document.querySelector('.input')
+
+button.addEventListener("click",()=>{
+    console.log(text.value);
+    sendurl(text.value)
+})
+let sendurl=(url)=>{
+    // fetch(`http://localhost/download?URL=${url}`,{
+    //     method:"GET"
+    // }).then(res=>res.json()).then(json=>{console.log(json)})
+    window.location.href=`http://localhost/download?URL=${url}`
+}
